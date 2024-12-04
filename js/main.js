91a0ff53b26e2867ea5c479bdc96ae1c8fb2365a
raevmaksim <raev_maksim_frontend@mail.ru>
2024-12-04 23:15:28 +0500

    mobile & pad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+//    Мобильное меню
+burger.onclick = function (e) { 
+  e.preventDefault()
+  // document.getElementById('navmenu').classList.remove('header__navmenu')
+  // document.getElementById('navmenu').classList.add('header__navmenu_mobile')
+    
+  document.getElementById('navMenu').style.display='block'
+  document.getElementById('menuBtns').style.display='block'
+  document.getElementById('burger').style.display='none'
+  document.getElementById('close').style.display='block'
+}
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -10,4 +22,24 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 1,
+    },
+    706: {
+      slidesPerView: 2,
+    },
+    1060: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
+  }
 });
